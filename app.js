4e01901cf1e41569a06f278d98ce1da7071d76a4
Porry Chen <porrychen@localhost.(none)>
2012-01-09 20:01:36 +0800

    add show.jade,用于显示上传的图片

--- app.js
+++ app.js
@@ -5,12 +5,16 @@
 
 var express = require('express'),
     routes = require('./routes'),
-    form = require('./multiparser'),
-    url = require('url'),
+    formidable = require('./multiparser'),
     config = require('./config'),
-    util = require('util');
+    fs = require('fs');
 
-var app = module.exports = express.createServer();
+var app = module.exports = express.createServer(
+    // connect-form (http://github.com/visionmedia/connect-form)
+    // middleware uses the formidable middleware to parse urlencoded
+    // and multipart form data
+    formidable({ keepExtensions: true, uploadDir: './data'})
+);
 
 //function(req, res){
 //    var uri = req.url;//url.parse(req.url).pathname;
@@ -40,45 +44,46 @@ var app = module.exports = express.createServer();
 
 // Configuration
 
-//app.configure(function(){
+app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-//  app.use(express.bodyParser());
-//  app.use(express.methodOverride());
-//  app.use(express.cookieParser());
-//  app.use(express.session({ secret: 'your secret here' }));
-//  app.use(app.router);
-//  app.use(express.static(__dirname + '/public'));
-//});
-//
-//app.configure('development', function(){
-//  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-//});
-//
-//app.configure('production', function(){
-//  app.use(express.errorHandler());
-//});
+  app.use(express.bodyParser());
+  delete express.bodyParser.parse['multipart/form-data'];
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'your secret here' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
 
 // Routes
 
 app.get('/', routes.index);
 app.post('/upload', function(req, res, next){
-    form({ keepExtensions: true })
-//    req.form.complete(function(err, fields, files){
-//        if (err) {
-//            next(err);
-//        } else {
-//            console.log('\nuploaded %s to %s'
-//                ,  files.image.filename
-//                , files.image.path);
-//            res.redirect('back');
-//        }
-//    });
+    req.form.complete(function(err, fields, files){
+        if (err) {
+            next(err);
+        } else {
+            console.log('\nuploaded %s to %s'
+                ,  files.upload.filename
+                , files.upload.path);
+            fs.renameSync(files.upload.path, './public/images/test.gif');
+            res.render('show', {title: 'Show Image ', name: files.upload.filename, path: 'images/test.gif'});
+        }
+    });
 
-//    req.form.on('progress', function(bytesReceived, bytesExpected){
-//        var percent = (bytesReceived / bytesExpected * 100) | 0;
-//        process.stdout.write('Uploading: %' + percent + '\r');
-//    });
+    req.form.on('progress', function(bytesReceived, bytesExpected){
+        var percent = (bytesReceived / bytesExpected * 100) | 0;
+        process.stdout.write('Uploading: %' + percent + '\r');
+    });
 });
 
 app.listen(config.port);
